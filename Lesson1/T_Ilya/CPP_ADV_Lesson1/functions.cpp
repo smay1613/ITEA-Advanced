f145eeb2e9b266fb6c5e9d615ed3a76f086d9ded
selestiel <selarch2018@gmail.com>
2019-05-20 09:55:42 +0300

    rev1 final

--- Lesson1/T_Ilya/CPP_ADV_Lesson1/functions.cpp
+++ Lesson1/T_Ilya/CPP_ADV_Lesson1/functions.cpp
@@ -2,10 +2,11 @@
 #include "task.h"
 
 namespace MFun{
-void print_vect(std::vector<int> a_vect){
-    for(MySpace::ull i{0}; i < a_vect.size(); i++){
-        std::cout << a_vect[i] << " , ";
-    }std::cout << std::endl;
+void print_vect(std::vector<int>& a_vect){
+    for(const int& value: a_vect){
+        std::cout << value << " , ";
+    }
+    std::cout << std::endl;
 
 }
 int divide_first(int x){
@@ -32,33 +33,28 @@ void print_vect_b(const std::vector<int>& vect_c){
     }
     std::cout << std::endl;
 }
-void showlist_z(std::list <int> list_z){
+void showlist_z(std::list<int>& list_z){
     for(const auto& value: list_z)
     {
         std::cout << '\t' << value;
-        std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
 void fill_list(std::list<int>& list_y, std::vector<int>& vect_y){
     list_y = std::list<int>(vect_y.cbegin(), vect_y.cend());
 }
-void showlist_fwd(std::forward_list <int> list_n)
+void showlist_fwd(std::forward_list<int>& list_n)
 {
-    std::forward_list <int> :: iterator it;
-    for(it = list_n.begin(); it != list_n.end(); ++it){
-        std::cout << ' ' << *it << std::endl;
+    for(const auto& value: list_n){
+        std::cout << '\t' << value;
     }
+    std::cout << std::endl;
 }
-void print_vect_a(std::vector<int> a_vect){
-    for(MySpace::ull i{0}; i < a_vect.size(); i++){
-        std::cout << a_vect[i] << " , ";
-    }std::cout << std::endl;
+void print_vect_a(std::vector<int>& a_vect){
+    for(const int& value: a_vect){
+        std::cout << value << " , ";
+    }
+    std::cout << std::endl;
 
 }
-
-/*void set_vector(std::vector<int> set_v)
-{
-    set_v.push_back(V.data);
-}
-*/
-}
+} // namespace MFun
--- Lesson1/T_Ilya/CPP_ADV_Lesson1/functions.h
+++ Lesson1/T_Ilya/CPP_ADV_Lesson1/functions.h
@@ -1,21 +1,19 @@
 #pragma once
 #include <list>
-#include <iterator>
 #include <forward_list>
 #include <iostream>
 #include <vector>
 
 namespace MFun{
 
-void print_vect(std::vector<int> a_vect);
+void print_vect(std::vector<int>& a_vect);
 int divide_first(int x);
 int divide_last(int y);
 void power(int c, int d, std::vector<int>& vect_c);
 void print_vect_b(const std::vector<int>& vect_c);
-void showlist_z(std::list <int> list_z);
+void showlist_z(std::list <int>& list_z);
 void fill_list(std::list<int>& list_y, std::vector<int>& vect_y);
-void showlist_fwd(std::forward_list <int> list_n);
-void print_vect_a(std::vector<int> a_vect);
-void set_vector(std::vector<int> set_v);
+void showlist_fwd(std::forward_list<int>& list_n);
+void print_vect_a(std::vector<int>& a_vect);
+void set_vector(std::vector<int>& set_v);
 }
-
