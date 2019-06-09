3f59b4e5e726ecfe32670a92f4545736cdb39267
Ph Skr <skrypskii.ph@gmail.com>
2019-06-10 00:25:16 +0300

    add lesson4 hometask

---
+++ Lesson4Hometask/PalindromeMVC/content.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QtWidgets>
+
+#include "palindromemodel.h"
+
+class Content : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Content(QWidget* parent = nullptr);
+
+    void processFile(const QString& fileName);
+    void processText();
+
+private:
+    QGroupBox* generateGroupBox(const QString& boxName, QWidget* widget) const;
+
+    void logMessage(const QString& message);
+
+private:
+    QTextEdit* m_textEdit;
+
+    QTableView* m_palindromeView;
+    PalindromeModel* m_palindromeModel;
+
+    QTextEdit* m_logEdit;
+
+    QCheckBox* checkBoxOneSymbol;
+    QCheckBox* checkBoxTwoSymbols;
+    QCheckBox* checkBoxCaseSensitive;
+
+signals:
+    void statusMessage(const QString&, int timeout = 10000);
+};
