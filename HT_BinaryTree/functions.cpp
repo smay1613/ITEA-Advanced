18c48231669d222ce6267f5c39cd7c57a86190e9
selestiel <selarch2018@gmail.com>
2019-06-16 23:07:51 +0300

    some more fix to BT

--- HT_BinaryTree/functions.cpp
+++ HT_BinaryTree/functions.cpp
@@ -6,12 +6,12 @@ void show_tree(node* head)
     std::queue<node*> list_tree;
     list_tree.push(head);
     while(true){
-    int vect = list_tree.size();
-    if(vect == 0){
+    size_t sub_value = list_tree.size();
+    if(sub_value == 0){
         break;
     }
-    int tabs = 0;
-    while(tabs<vect){
+    size_t tabs = 0;
+    while(tabs<sub_value){
         node* num = list_tree.front();
         std::cout << num->value << " ";
         if(num->left != 0){
