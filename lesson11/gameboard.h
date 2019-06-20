055e2f2b43c79a594197fd578fea0690d16cb4cd
Philipp Skrupskiy <skrypskii.ph@gmail.com>
2019-06-20 18:41:17 +0300

    upd: step to gameboard abstractListModel

--- lesson11/gameboard.h
+++ lesson11/gameboard.h
@@ -4,6 +4,7 @@
 #include <QAbstractListModel>
 
 // homework : to MVC - model ListModel
+// https://www.youtube.com/watch?v=9BcAYDlpuT8&t=1215s
 
 class GameBoard : public QAbstractListModel
 {
@@ -11,7 +12,7 @@ class GameBoard : public QAbstractListModel
 public:
     static constexpr size_t DEFAULT_DIMENSION  {4};
 
-    GameBoard(size_t board_dimension = DEFAULT_DIMENSION);
+    GameBoard(QObject *parent = nullptr, size_t board_dimension = DEFAULT_DIMENSION);
 
     struct Tile {
         size_t value {};
@@ -22,6 +23,24 @@ public:
     };
 
     void shuffle();
+
+    //{ To become a QAbstractListModel
+
+    enum {
+        someRole = Qt::UserRole,
+        anotherRole
+    };
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+    virtual QHash<int, QByteArray> roleNames() const override;
+    //}
 
 private:
     std::vector<Tile> m_raw_board;
