5c6fbded37bd095fc6728210aa6ae6b07ff840d0
Holodenko_Artem <31826517+Acnologiak@users.noreply.github.com>
2019-07-31 19:23:44 +0300

    Update main.cpp

--- Lesson2/Holodenko_Artem/task_1/main.cpp
+++ Lesson2/Holodenko_Artem/task_1/main.cpp
@@ -2,61 +2,156 @@
 #include <stack>
 #include <string>
 
-void print_stack(std::stack<char> stack)
+
+struct b_type
 {
-    while(!stack.empty())
+    b_type(size_t _type) : type(_type)
     {
-        std::cout << stack.top() << " ";
-        stack.pop();
-    }
-    std::cout << std::endl;
-}
 
-std::stack<char> string_to_stack(std::string *_str)
+    }
+    size_t type;
+    size_t number = 1;
+};
+
+bool is_expression_valid(const std::string &str)
 {
-    std::stack<char> data_stack;
-    const char* str = _str->c_str();
-    const size_t size =  _str->size();
-
-    for (size_t i=0; i< size; i++)
-    {
-        data_stack.push(str[size-i-1]);
-    }
-
-    return data_stack;
-}
-
-bool func(std::stack<char> stack)
-{
-    int x = 0;
-
-    while(!stack.empty())
-    {
-        if (stack.top()=='(')
-        {
-            x++;
-        }
-        else if (stack.top()==')')
-        {
-            x--;
-        }
-
-        //std::cout << x << std::endl;
-
-        if (x < 0)
-        {
-            return false;
-        }
-
-        stack.pop();
-    }
-    if (x!=0)
+    std::stack<b_type> data_stack;
+    if (str.empty())
     {
         return false;
     }
     else
     {
-        return true;
+        for (int i=0; i<str.size(); i++)
+        {
+            switch (str[i])
+            {
+                case '(':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 0)
+                        {
+                            data_stack.top().number++;
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{0});
+                        }
+                    }
+                    else
+                    {
+                        data_stack.emplace(b_type{0});
+                    }
+                break;
+                case ')':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 0)
+                        {
+                            data_stack.top().number--;
+                            if (data_stack.top().number == 0)
+                            {
+                                data_stack.pop();
+                            }
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{0});
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                break;
+                case '[':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 1)
+                        {
+                            data_stack.top().number++;
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{1});
+                        }
+                    }
+                    else
+                    {
+                        data_stack.emplace(b_type{1});
+                    }
+                break;
+                case ']':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 1)
+                        {
+                            data_stack.top().number--;
+                            if (data_stack.top().number == 0)
+                            {
+                                data_stack.pop();
+                            }
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{1});
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                break;
+                case '{':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 2)
+                        {
+                            data_stack.top().number++;
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{2});
+                        }
+                    }
+                    else
+                    {
+                        data_stack.emplace(b_type{2});
+                    }
+                break;
+                case '}':
+                    if (!data_stack.empty())
+                    {
+                        if (data_stack.top().type == 2)
+                        {
+                            data_stack.top().number--;
+                            if (data_stack.top().number == 0)
+                            {
+                                data_stack.pop();
+                            }
+                        }
+                        else
+                        {
+                            data_stack.emplace(b_type{2});
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                break;
+            }
+        }
+
+        if (!data_stack.empty())
+        {
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
     }
 }
 
@@ -66,9 +161,7 @@ int main()
 
     std::cin >> data_str;
 
-    std::stack<char> data_stack {std::move(string_to_stack(&data_str))};
-    print_stack(data_stack);
-    std::cout << func(data_stack) << std::endl;
+    std::cout << is_expression_valid(data_str) << std::endl;
 
     return 0;
 }
