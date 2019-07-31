d6741f1bf2523714d1c111186dd46664d34ce1fa
Sielim <48602792+SielimDDk@users.noreply.github.com>
2019-07-31 22:24:30 +0300

    Home task 1.3(revers of vector)

---
+++ Lesson1/Selim_Osmanov/HomeTask1.3/ht1_3head.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <forward_list>
+
+void EnterElements (std::forward_list<int>& MyForwardList);
+void ShowElements (std::forward_list<int>& MyForwardList);
+void ReverseElements (std::forward_list<int>& MyForwardList,
+                      std::forward_list<int>& MyNewForwardList);
