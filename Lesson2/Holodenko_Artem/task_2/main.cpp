9ce305cf4864e8912f82a37734b9e3aba39ac18c
Holodenko_Artem <31826517+Acnologiak@users.noreply.github.com>
2019-08-05 11:29:29 +0300

    Update main.cpp

--- Lesson2/Holodenko_Artem/task_2/main.cpp
+++ Lesson2/Holodenko_Artem/task_2/main.cpp
@@ -1,175 +1,138 @@
 #include <iostream>
+#include <deque>
 #include <queue>
-#include <vector>
 #include <math.h>
+
 
 struct node
 {
+    node (int _n)
+        : n(_n)
+    {
+
+    }
+    node (int _n, node *_left, node *_right)
+        : n(_n),
+          left(_left),
+          right(_right)
+    {
+
+    }
     int n = 0;
     struct node *left = nullptr;
     struct node *right = nullptr;
     struct node *level = nullptr;
 };
 
-
-std::vector<node> get_data()
+void create_tree(std::deque<node> &_tree, node* element)
 {
-    int number_levels, number_nodes=0;
-    std::cout << "Enter number of levels: ";
-    std::cin >> number_levels;
+    int n;
 
-    //кількість вузлів
-    for (int i=0; i<number_levels; i++) number_nodes+=pow(2, i);
-
-    //std::cout << number_nodes << std::endl;
-    std::vector<node> data_vector (number_nodes);
-
-    //заповнення дерева, 0 пустий вузол
-    for (int i=0; i<number_nodes; i++)
+    std::cout << element->n << "->";
+    std::cin >> n;
+    if (n!=0)
     {
-        std::cout << "Enter node " << i+1 << ": ";
-        std::cin >> data_vector[i].n;
+        _tree.push_back(node{n});
+        element->left = &_tree[_tree.size()-1];
+        create_tree(_tree, &_tree[_tree.size()-1]);
     }
 
-    //створення зв'язків
+    std::cout << element->n << "->";
+    std::cin >> n;
+    if (n!=0)
     {
-        int m, n;
-        int level;
-        for (int i=0; i<number_nodes; i++)
-        {
-            level = 0;
-            m=i+1;
-            while (m>pow(2, level))
-            {
-                m-=pow(2, level);
-                level++;
-            }
-            level++;
-
-            //std::cout << "level " << level << std::endl;
-            //std::cout << "m " << m << std::endl;
-
-            n = 0;
-            for (int j=0; j<level; j++) n+=pow(2, j);
-            n+=(m-1)*2;
-
-            //std::cout << "n1 " << n << std::endl;
-
-            if (n<number_nodes and data_vector[n].n!=0)
-            {
-                if (data_vector[i].n==0)
-                {
-                    data_vector[n].n=0;
-                }
-                else
-                {
-                    data_vector[i].left=&data_vector[n];
-                }
-            }
-
-            n = 0;
-            for (int j=0; j<level; j++) n+=pow(2, j);
-            n+=(m-1)*2+1;
-
-            //std::cout << "n2 " << n << std::endl;
-
-            if (n<number_nodes and data_vector[n].n!=0)
-            {
-                if (data_vector[i].n==0)
-                {
-                    data_vector[n].n=0;
-                }
-                else
-                {
-                    data_vector[i].right=&data_vector[n];
-                }
-            }
-        }
-    }
-
-    return  data_vector;
-}
-
-
-
-
-
-void print_node(std::vector<node> &data_vector)
-{
-    for (int i=0; i<data_vector.size(); i++)
-    {
-        if (data_vector[i].n == 0) continue;
-        std::cout << "node " << data_vector[i].n << ":" <<std::endl;
-        if (data_vector[i].left!=nullptr)
-        {
-            std::cout << "- " << data_vector[i].left->n << std::endl;
-        }
-        if (data_vector[i].right!=nullptr)
-        {
-            std::cout << "- " << data_vector[i].right->n << std::endl;
-        }
-        if (data_vector[i].level!=nullptr)
-        {
-            std::cout << "- " << data_vector[i].level->n << std::endl;
-        }
+        _tree.push_back(node{n});
+        element->right = &_tree[_tree.size()-1];
+        create_tree(_tree, &_tree[_tree.size()-1]);
     }
 }
 
-
-void func(std::queue<node*> data_queue)
+void create_tree(std::deque<node> &_tree)
 {
-    int n=0;
-    int level=0;
-    node *y = nullptr;
-    while(!data_queue.empty())
-    {
-        n++;
+    int n;
+    std::cout << "->";
+    std::cin >> n;
+    _tree.push_back(node{n});
+    create_tree(_tree, &_tree[0]);
+}
 
-        if (data_queue.front()->n!=0)
+void print_nodes(node* element)
+{
+    if (element->left!=nullptr)
+    {
+        std::cout << element->n << "->" << element->left->n <<std::endl;
+    }
+    if (element->right!=nullptr)
+    {
+        std::cout << element->n <<"->" << element->right->n <<std::endl;
+    }
+    if (element->level!=nullptr)
+    {
+        std::cout << element->n <<"->" << element->level->n <<std::endl;
+    }
+
+    if (element->left!=nullptr)
+    {
+        print_nodes(element->left);
+    }
+    if (element->right!=nullptr)
+    {
+        print_nodes(element->right);
+    }
+}
+
+void linkLevelNodes(node* element)
+{
+    std::queue<node*> nodes_queue;
+    node* element_level_1 = nullptr;
+    node* element_level_2 = element->left;
+
+    nodes_queue.push(element);
+
+    while (!nodes_queue.empty())
+    {
+        if (nodes_queue.front()->left!=nullptr)
         {
-            if (y==nullptr)
+            nodes_queue.push(nodes_queue.front()->left);
+        }
+        if (nodes_queue.front()->right!=nullptr)
+        {
+            nodes_queue.push(nodes_queue.front()->right);
+        }
+
+        if (element_level_1 == nullptr)
+        {
+            element_level_1 = nodes_queue.front();
+        }
+        else
+        {
+            if (nodes_queue.front() == element_level_2)
             {
-                y = data_queue.front();
+                element_level_1 = nodes_queue.front();
+                element_level_2 = nodes_queue.front()->left;
             }
             else
             {
-                y->level=data_queue.front();
-                y = data_queue.front();
-
+                element_level_1->level = nodes_queue.front();
+                element_level_1 = nodes_queue.front();
             }
         }
-
-        if (n==pow(2, level))
-        {
-            y = nullptr;
-            level++;
-            n=0;
-        }
-
-        data_queue.pop();
+        nodes_queue.pop();
     }
 }
 
-
 int main()
 {
-    std::vector<node> data_vector;
-    std::queue<node*> data_queue;
+    std::deque<node> tree;
+    create_tree(tree);
 
-    data_vector = std::move(get_data());
+    std::cout <<std::endl;
+    print_nodes(&tree[0]);
 
-    std::cout << std::endl;
-    print_node(data_vector);
+    linkLevelNodes(&tree[0]);
 
-    for (int i=0; i<data_vector.size(); i++)
-    {
-        data_queue.push(&data_vector[i]);
-    }
+    std::cout <<std::endl;
+    print_nodes(&tree[0]);
 
-    func(data_queue);
-
-    std::cout << std::endl;
-    print_node(data_vector);
-
-    return 0;
+    return 1;
 }
