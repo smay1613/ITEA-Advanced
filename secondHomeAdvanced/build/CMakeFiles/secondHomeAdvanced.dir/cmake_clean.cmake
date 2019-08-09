425c2ab538073546c868de7cf8f42ac1b47399b9
futurestark <ischenkoVitaly@gmail.com>
2019-08-09 20:34:54 +0300

    added second home

---
+++ secondHomeAdvanced/build/CMakeFiles/secondHomeAdvanced.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secondHomeAdvanced.dir/secondHomeAdvanced.cpp.o"
+  "libsecondHomeAdvanced.pdb"
+  "libsecondHomeAdvanced.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secondHomeAdvanced.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ secondHomeAdvanced/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jarvis/secondHomeAdvanced
+# Build directory: /home/jarvis/secondHomeAdvanced/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")
