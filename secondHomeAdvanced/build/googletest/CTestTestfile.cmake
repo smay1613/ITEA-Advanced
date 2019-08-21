ced332cd6a48ff17467dba33cb9c1613ba004ef4
futurestark <ischenkoVitaly@gmail.com>
2019-08-21 14:41:49 +0300

    cleaned

--- secondHomeAdvanced/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jarvis/secondHomeAdvanced/googletest
-# Build directory: /home/jarvis/secondHomeAdvanced/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- secondHomeAdvanced/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jarvis/secondHomeAdvanced/test
-# Build directory: /home/jarvis/secondHomeAdvanced/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ExampleTests "/home/jarvis/secondHomeAdvanced/build/test/ExampleTests")
