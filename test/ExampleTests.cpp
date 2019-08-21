55a6b88263b4b9005766ccf878a22657be0d6abb
futurestark <ischenkoVitaly@gmail.com>
2019-08-21 14:38:46 +0300

    deleted garrbage files

--- test/ExampleTests.cpp
+++ test/ExampleTests.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "../firstHomeAdvanced.hpp"
+#include "../secondHomeAdvanced.hpp"
 
 //using ::testing::Test; 
 //------------------ usual two tests -----------------------
@@ -28,83 +28,43 @@ TEST(ExampleTests, DemonstrateGTestMacros){
      EXPECT_EQ(true, result) << "i = " << i << std::endl;
     */
 
-TEST(ExampleTests, combineVectors){ 
-    std::vector<int> firstVector{ 5, 6, 7, 8, 9 };
-	std::vector<int> secondVector{ 12, 13, 14, 15, 16 };
 
-	std::vector<int> resultVector{ 0 };
 
-	std::vector<int>::iterator itFirstVecBeg = firstVector.begin();
-	std::vector<int>::iterator itFirstVecEnd = firstVector.end();
-	std::vector<int>::iterator itSecondVecBeg = secondVector.begin();
-	std::vector<int>::iterator itSecondVecEnd = secondVector.end();
-
-	//resultVector.resize(firstVector.size() + secondVector.size());
-	//resultVector.reserve(firstVector.size() + secondVector.size());
-	resultVector = combineVectors(itFirstVecBeg, itFirstVecEnd, itSecondVecBeg, itSecondVecEnd);
-
-	std::cout << "resultVectorSize " << resultVector.size() << std::endl;
-
-	for (auto i : resultVector) { // access by value, type of i is int
-		std::cout << i << ' ';
-		std::cout << '\n';
-	}
-
-    //EXPECT_THAT(resultVector, ::testing::UnorderedElementsAreArray(testVector)); 
-    ASSERT_THAT(resultVector, ::testing::ElementsAre(5, 6, 7, 8, 9, 12, 13, 14, 15, 16));
-}
-
-TEST(ExampleTests, reverseFrowardList){ 
+// regex has search, match and verifying methods for strings
+TEST(ExampleTests, isExpressionValid){ 
    
-	std::forward_list<int> myforwardlist = { 1, 2, 3, 4, 5, 6, 7, 8 }, resultList;
-    std::forward_list<int> & linkForwardList = myforwardlist;
-	// myforwardlist.push_front(8);  need push front elements in cycle like this
+   std::string expressionInString = {"()()))"};
+   std::string & linkToStr = expressionInString;
+   bool expresionIsValid;
 
-	resultList = reverseFrowardList(linkForwardList);
+   expresionIsValid = isExpressionValid(linkToStr);
 
-	std::cout << std::endl;
-	std::cout << "	reversed forward list is : " << std::endl;
-	for (int i : resultList)
-	{
-		std::cout << i << " ";
-	}
+   EXPECT_EQ(false, expresionIsValid)  << std::endl;
 
-    //EXPECT_THAT(resultVector, ::testing::UnorderedElementsAreArray(testVector)); 
-    ASSERT_THAT(resultList, ::testing::ElementsAre(8, 7, 6, 5, 4, 3, 2, 1));
+   expressionInString = {"{}{}{}"};
+   expresionIsValid = isExpressionValid(linkToStr);
+
+   EXPECT_EQ(true, expresionIsValid)  << std::endl;
+
 }
 
-TEST(ExampleTests,createNumber){ 
+TEST(ExampleTests, linkLevelNodes){ 
    
-	int value = 5, powOfValue = 6;
-	std::cout << std::endl;
-	//std::cout << "TASK 2 - creation number using list " << std::endl;
-	//std::cout << std::endl;
-	//std::cout << "	enter value to be pow : " << std::endl;
-	//std::cin >> value;
-	//std::cout << "	enter max pow for value: " << std::endl;
-	//std::cin >> powOfValue;
+    Node* binaryTree = nullptr;
+    
+    binaryTree = new Node(15);
+    binaryTree->left = new Node(10);
+    binaryTree->right = new Node(20);
+    binaryTree->left->left = new Node(8);
+    binaryTree->left->right = new Node(12);
+    binaryTree->right->left = new Node(16);
+    binaryTree->right->right = new Node(25);
 
-	std::list<std::string> valuesList;
+    linkLevelNodes(binaryTree);
 
-	valuesList = createNumber(value, powOfValue);
+    //EXPECT_EQ(false, expresionIsValid)  << std::endl;
+    //ASSERT_THAT(*binaryTree, ::testing::ElementsAre("15", "20", "10", "25", "16", "12", "8"));
 
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	//printList(valuesList);
-
-	std::cout << std::endl;
-
-    ASSERT_THAT(valuesList, ::testing::ElementsAre("1", "2", "5", "1", "5", "6", "5", "3", "5", "1", "5"));   //here first number must be 5 fpr pass test]
-}
-
-TEST(ExampleTests, Square){
-    int x = 5;
-    int expectedSquare = x * x;
-    EXPECT_EQ(
-        expectedSquare,
-        Square(x)
-    );
 }
 
 //---------------------- end usual two tests -----------------
