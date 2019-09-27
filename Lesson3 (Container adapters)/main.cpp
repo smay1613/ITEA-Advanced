ecb6cc3cbc45743fdcf66a72f43b70dc4055c9ca
Anyo822 <anyodk@gmail.com>
2019-09-27 19:02:09 +0300

    added 2nd task

--- Lesson3 (Container adapters)/main.cpp
+++ Lesson3 (Container adapters)/main.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <stack>
+#include <queue>
 
 bool isExpressionValid (const std::string& expression)
 {
@@ -32,12 +33,84 @@ bool isExpressionValid (const std::string& expression)
     return receivedMessage.empty();
 }
 
-//void linkLevelNodes(node* (или std::shared_ptr<node> - *КРАЙНЕ ПРЕДПОЧТИТЕЛЬНЕЕ ЕГО ИСПОЛЬЗОВАНИЕ!*)
+struct node
+{
+    int n;
+    struct node *left;
+    struct node *right;
+    struct node *level;
+};
+
+
+int getNodeLevel (node* nodeToObserve, node* nodeTofind, int level)
+{
+    if(nodeToObserve == nullptr)
+    {
+        return 0;
+    }
+    if(nodeToObserve == nodeTofind)
+    {
+        return  level;
+    }
+
+    int downLevel = getNodeLevel(nodeToObserve->left, nodeTofind, level + 1);
+    if(downLevel != 0)
+    {
+        return downLevel;
+    }
+
+    downLevel = getNodeLevel(nodeToObserve->right, nodeTofind, level + 1);
+    return downLevel;
+
+}
+
+void linkLevelNodes(node* root)
+{
+    std::queue<node*> discoveredNodes;
+    discoveredNodes.push(root);
+
+    while(!discoveredNodes.empty())
+    {
+        node* current_node = discoveredNodes.front();
+        discoveredNodes.pop();
+        node* next_node = discoveredNodes.front();
+
+        int nodeLevelDifference = getNodeLevel(root, current_node, 0) - getNodeLevel(root, next_node, 0);
+
+        if(!discoveredNodes.empty() && nodeLevelDifference == 0)
+        {
+            current_node->level = next_node;
+        }
+        if (current_node->left != nullptr)
+        {
+            discoveredNodes.push(current_node->left);
+        }
+        if (current_node->right != nullptr)
+        {
+            discoveredNodes.push(current_node->right);
+        }
+
+        current_node = discoveredNodes.front();
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    //std::string input {"{{}[[()]()]{}}"};
-    //bool answer = isExpressionValid(input);
+    /*
+    std::string input {"{{}[[()]()]{}}"};
+    bool answer = isExpressionValid(input);
+
+    node a, b, c, d, e, f, g;
+    a = {1, &b, &c, nullptr};
+    b = {2, &d, &e, nullptr};
+    c = {3, nullptr, &f, nullptr};
+    d = {4, &g, nullptr, nullptr};
+    e = {5, nullptr, nullptr, nullptr};
+    f = {6, nullptr, nullptr, nullptr};
+    g = {7, nullptr, nullptr, nullptr};
+    node* tree_root = &a;
+    linkLevelNodes(tree_root);
+    */
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
